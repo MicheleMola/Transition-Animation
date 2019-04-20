2d77914056a7b335a76aca6795d6ea331c50bbc7
Michele Mola <michele.mola92@gmail.com>
2019-04-20 13:00:02 +0200

    Initial Commit

---
+++ TransitionAnimation2Approach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionAnimation2Approach
+//
+//  Created by Michele Mola on 20/04/2019.
+//  Copyright © 2019 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
